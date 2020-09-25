9e83ab059b9f4dde05ba49e9161b46b9a0f33862
unknown <akhileshmanuvarthi@gmail.com>
2020-09-25 20:49:22 +0530

    Added blogs component

--- src/components/grid-view/index.js
+++ src/components/grid-view/index.js
@@ -1,29 +1,22 @@
 import React from 'react'
 import {
   Grid,
-  Image,
+  Image
 } from 'semantic-ui-react'
+import {Blogs} from './Blogs'
+
 export const GridExampleVerticallyDivided = () => (
-    <Grid className='gridview' divided='vertically'>
-      <Grid.Row columns={2}>
-        <Grid.Column width={10}>
-          <Image src='https://via.placeholder.com/300.png/09f/fff' />
-        </Grid.Column>
-        <Grid.Column width={6}>
-          <Image src='https://via.placeholder.com/300.png/09f/fff' />
-        </Grid.Column>
-      </Grid.Row>
-  
-      <Grid.Row columns={3}>
-        <Grid.Column>
-          <Image src='https://via.placeholder.com/300.png/09f/fff' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='https://via.placeholder.com/300.png/09f/fff' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='https://via.placeholder.com/300.png/09f/fff' />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  )
+  <Grid className='gridview' divided='vertically'>
+    <Grid.Row columns={2}>
+      <Grid.Column width={10}>
+        <Image src='https://via.placeholder.com/300.png/09f/fff' />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <Image src='https://via.placeholder.com/300.png/09f/fff' />
+      </Grid.Column>
+    </Grid.Row>
+
+    {Blogs()}
+
+  </Grid>
+)
