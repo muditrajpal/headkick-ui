b1e7495847485e79aca5e2191b3656de30af75c3
Lawrence <Lawrence4code@gmail.com>
2020-11-09 09:28:11 +0530

    feat: create tournament overview section and start working on child components

--- src/views/TournamentOverview/TournamentOverview.js
+++ src/views/TournamentOverview/TournamentOverview.js
@@ -1,5 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Grid, Segment, Tab } from 'semantic-ui-react';
 
-const TournamentOverview = ()=><div>TournamentOverview</div>
+// internal imports
+import BlogSection from './../../components/BlogSection/BlogSection';
+import { TournamentOverviewContainer } from './TournamentOverview.styles';
+import {
+  onGoingMatchDummyData,
+  leagueOptions,
+  blogData,
+  overviewVideoInfo,
+} from './dummyData';
+import OnGoingMatchesRender from '../../components/OnGoingMatchesRender/OnGoingMatchesRender';
+
+const panes = [
+  {
+    menuItem: 'On going matches',
+    render: ({ onGoingMatches }) => {
+      const onGoingMatchesJSX = OnGoingMatchesRender(
+        onGoingMatches,
+        leagueOptions
+      );
+      return onGoingMatchesJSX;
+    },
+  },
+  {
+    menuItem: 'Upcoming matches',
+    render: () => <Tab.Pane>Upcoming matches content</Tab.Pane>,
+  },
+  {
+    menuItem: 'Post matches',
+    render: () => <Tab.Pane>Post matches content</Tab.Pane>,
+  },
+];
+
+const TournamentOverview = () => {
+  const [onGoingMatches, setOnGoingMatches] = useState([]);
+  useEffect(() => {
+    setOnGoingMatches(onGoingMatchDummyData);
+  }, []);
+  return (
+    <TournamentOverviewContainer>
+      <Grid stackable columns={2}>
+        <Grid.Column width={11}>
+          <Segment className="columnItem matchDashboard">
+            <Tab onGoingMatches={onGoingMatches} panes={panes} />
+          </Segment>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <Segment className="columnItem tournamentOverviewImage">
+            <div className="tournamentOverviewImageText">
+              <p className="tournamentOverviewImageTextTitle">
+                {overviewVideoInfo[0].videoTitle}
+              </p>
+              <p className="tournamentOverviewImageTextSubtitle">
+                {overviewVideoInfo[0].videoSubtitle}
+              </p>
+            </div>
+            <div className="tournamentOverviewImageIcon">
+              <span> Play Icon </span>
+            </div>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+      <BlogSection blogData={blogData}></BlogSection>
+    </TournamentOverviewContainer>
+  );
+};
 
 export default TournamentOverview;
---
+++ src/views/TournamentOverview/dummyData.js
@@ -0,0 +1,89 @@
+export const onGoingMatchDummyData = [
+  {
+    leagueStatus: 'Live',
+    leagueName: 'English Premier League',
+    teamOne: 'Barcelona',
+    teamTwo: 'Arsenal',
+    teamOneLogo: 'https://via.placeholder.com/75',
+    teamTwoLogo: 'https://via.placeholder.com/75',
+    teamOneGoals: 2,
+    teamTwoGoals: 1,
+    matchReferee: 'Jack Paretta',
+    matchTime: '30:00',
+    matchTimelineLink: 'https://somelinkhere.com',
+  },
+];
+
+export const leagueOptions = [
+  {
+    key: 'epl',
+    text: 'Premier League',
+    value: 'EPL',
+    image: {
+      avatar: true,
+      src:
+        'https://yt3.ggpht.com/a/AATXAJznyYl94RkgGa-dr-rsJAAgufjqxY-uLa-21pxy=s900-c-k-c0x00ffffff-no-rj',
+    },
+  },
+  {
+    key: 'll',
+    text: 'LaLiga',
+    value: 'LL',
+    image: {
+      avatar: true,
+      src:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQkbr4sp5MzSLRQnuxkCpbmdSU6UsQz9YpSA&usqp=CAU',
+    },
+  },
+  {
+    key: 'sa',
+    text: 'Serie A',
+    value: 'SA',
+    image: {
+      avatar: true,
+      src:
+        'https://lh3.googleusercontent.com/ktpS0TBR93beQNnMvoY6OCzeY89TW_KbToCV7H0gcUOSNuwO9BIGqwLLF-tfNhGyVhcQflKolEUkScekTYXkZhE',
+    },
+  },
+];
+
+export const blogData = [
+  {
+    id: '5f6a2cbc9c42baf7007d9a18',
+    title: 'laboris aliquip elit',
+    time: 9,
+    author: 'Patrick House',
+    authorImage: 'https://randomuser.me/api/portraits/men/75.jpg',
+    articleImage: 'https://via.placeholder.com/360x160',
+    lead:
+      'Est cillum ad eiusmod id labore deserunt incididunt culpa non nulla. Laborum culpa et excepteur aliquip proident id sint dolor ut sunt excepteur. Adipisicing laborum minim officia nostrud adipisicing cillum exercitation proident eu elit amet.\r\n',
+  },
+  {
+    id: '5f6a2cbca80484c3bf55a42e',
+    title: 'ad et labore',
+    time: 4,
+    author: 'Elvia Tillman',
+    authorImage: 'https://randomuser.me/api/portraits/women/75.jpg',
+    articleImage: 'https://via.placeholder.com/360x160',
+    lead:
+      'Magna ipsum voluptate labore esse ut labore reprehenderit deserunt occaecat. Voluptate qui do mollit enim adipisicing esse exercitation nostrud. Qui eiusmod proident consectetur veniam voluptate aliqua. Labore nisi minim non non ea nisi non ullamco mollit reprehenderit do fugiat.\r\n',
+  },
+  {
+    id: '5f6a2cbc7ec12129b3b29918',
+    title: 'mollit ullamco est',
+    time: 7,
+    author: 'Ruby Boyd',
+    authorImage: 'https://randomuser.me/api/portraits/women/45.jpg',
+    articleImage: 'https://via.placeholder.com/360x160',
+    lead:
+      'Elit fugiat eiusmod Lorem ipsum commodo exercitation. Sint adipisicing non in dolore tempor deserunt est ullamco commodo ea adipisicing. Duis sit irure fugiat aliquip qui. Laboris dolore eiusmod laboris laboris officia sit et officia est sint ad. Nostrud do excepteur officia quis id irure laboris fugiat et et veniam et aliquip. Non qui aute nulla aliqua eiusmod labore tempor exercitation sunt occaecat.\r\n',
+  },
+];
+
+// array so that list of video and can be randomized
+export const overviewVideoInfo = [
+  {
+    videoTitle: 'Barcelona Match',
+    videoSubtitle: 'Lionel Messi',
+  },
+];
