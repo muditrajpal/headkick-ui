60d256ec89f8c3f40110030c93f9eda19936093e
Lawrence <Lawrence4code@gmail.com>
2020-11-26 09:48:32 +0530

    create modals for sign and signup along with forms input

---
+++ src/components/SignUpModals/index.js
@@ -0,0 +1,4 @@
+import SignUpProfileModal from './SignUpProfileModal/SignUpProfileModal';
+import SignUpFormModal from './SignUpFormModal/SignUpFormModal'
+
+export {SignUpProfileModal, SignUpFormModal};
